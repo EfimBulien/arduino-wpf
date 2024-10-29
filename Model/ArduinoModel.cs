21582a34a314c423992171796fd579fb7d5e3d29
Ефим Бульен <143902970+EfimBulien@users.noreply.github.com>
2024-10-29 18:49:34 +0300

    mvvm

---
+++ Model/ArduinoModel.cs
@@ -0,0 +1,31 @@
+﻿using System.IO.Ports;
+
+namespace ArduinoWPF
+{
+    public class ArduinoModel
+    {
+        private SerialPort _serialPort;
+
+        public ArduinoModel(string portName)
+        {
+            _serialPort = new SerialPort(portName, 9600);
+            _serialPort.Open();
+        }
+
+        public void SendCommand(string command)
+        {
+            if (_serialPort.IsOpen)
+            {
+                _serialPort.WriteLine(command);
+            }
+        }
+
+        public void Close()
+        {
+            if (_serialPort.IsOpen)
+            {
+                _serialPort.Close();
+            }
+        }
+    }
+}
---
+++ ViewModel/MainViewModel.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.IO.Ports;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Windows.Input;
+
+namespace ArduinoWPF
+{
+    public class MainViewModel : INotifyPropertyChanged
+    {
+        private string _labelContent;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        public string LabelContent
+        {
+            get => _labelContent;
+            set
+            {
+                _labelContent = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public ICommand Button1Command { get; }
+
+        public ICommand Button2Command { get; }
+
+        public ICommand Button3Command { get; }
+
+        public ICommand S1Command { get; }
+
+        public ICommand S2Command { get; }
+
+        public ObservableCollection<string> Frequencies { get; set; }
+
+        public string SelectedFrequency { get; set; }
+
+        private ArduinoModel _arduinoModel;
+
+        public MainViewModel()
+        {
+            Frequencies = new ObservableCollection<string> { "0.1 sec", "0.5 sec", "1 sec" };
+            SelectedFrequency = Frequencies.First();
+            Button1Command = new RelayCommand(OnButton1);
+            Button2Command = new RelayCommand(OnButton2);
+            Button3Command = new RelayCommand(OnButton3);
+            S1Command = new RelayCommand(OnS1);
+            S2Command = new RelayCommand(OnS2);
+            InitializeArduinoConnection("COM3");
+        }
+
+        private void InitializeArduinoConnection(string portName)
+        {
+            if (SerialPort.GetPortNames().Contains(portName)) _arduinoModel = new ArduinoModel(portName);
+            else Console.WriteLine($"Порт '{portName}' не существует или недоступен.");
+        }
+
+        private void OnButton1()
+        {
+            _arduinoModel.SendCommand($"BLINK:{SelectedFrequency}");
+        }
+
+        private void OnButton2()
+        {
+            _arduinoModel.SendCommand("PLAY_IMPERIAL_MARCH");
+        }
+
+        private void OnButton3()
+        {
+            _arduinoModel.SendCommand("STOP_MUSIC");
+        }
+
+        private void OnS1()
+        {
+            LabelContent = "Нажата первая кнопка";
+        }
+
+        private void OnS2()
+        {
+            LabelContent = "Нажата вторая кнопка";
+        }
+    }
+}
